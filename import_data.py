17c6aaa524d4599d40c0022837220c74ca191a77
ghosinski <ghosinski@icloud.com>
2020-07-26 18:10:37 -0700

    created clean data set movie_data.csv for database usage

--- import_data.py
+++ import_data.py
@@ -8,9 +8,12 @@ df = pd.read_csv('MoviesOnStreamingPlatforms_updated.csv',index_col=0)
 df['Rotten Tomatoes'] = df['Rotten Tomatoes'].str.rstrip('%').astype('float') / 100.0
 df.dropna(subset = ['IMDb','Runtime','Genres'], inplace=True)
 df.Age.fillna('18+', inplace=True)
-df['Genres'] = df.Genres.apply(lambda x: x.split(','))
+
+# df['Genres'] = df.Genres.apply(lambda x: x.split(',')) # this was used for counting the number of genres
 # Split off test set
 df_test = df[df.ID <= 100].reset_index(drop=True)
+
+df.to_csv('movie_data.csv',index=False)
 
 # ------------------------------------------- #
 
@@ -34,7 +37,7 @@ print("Missing values per column:")
 print(df.apply(num_missing, axis=0))
 
 # Counting number of genres 
-Counts = pd.Series(sum([item for item in df.Genres], [])).value_counts()
+# Counts = pd.Series(sum([item for item in df.Genres], [])).value_counts() #This was used for counting genres
 
 # -------------------------------- # 
 
