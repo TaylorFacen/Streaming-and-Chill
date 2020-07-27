48b9421827d816253cbc649ed4fe7a53501d32b7
TaylorFacen <44216029+TaylorFacen@users.noreply.github.com>
2020-07-26 21:36:25 -0400

    Added app skeleton

---
+++ src/containers/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <h1>Streaming and Chill</h1>
+      <p>Made by Team 5</p>
+    </main>
+  );
+}
+
+export default App;
