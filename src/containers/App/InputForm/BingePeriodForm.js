5d5962705b082a305a227b2b822649d50d3a248b
TaylorFacen <44216029+TaylorFacen@users.noreply.github.com>
2020-07-29 08:37:26 -0400

    Added the Binge Period form

--- src/containers/App/InputForm/BingePeriodForm.js
+++ src/containers/App/InputForm/BingePeriodForm.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 
-export default ({ onSubmit }) => (
+export default ({ dayCount, hourCount, onChange, onSubmit }) => (
     <div className = "BingePeriodForm">
-        Binge Period
+        <h2>Binge Period</h2>
+        <Form.Group controlId="dayCount">
+            <Form.Label>How many days is your binge?</Form.Label>
+            <Form.Control 
+                type = "number" 
+                value = { dayCount }
+                name = "dayCount"
+                onChange = { onChange }
+            />
+        </Form.Group>
+        <Form.Group controlId="hourCount">
+            <Form.Label>How many hours per day do you want to spend watching movies?</Form.Label>
+            <Form.Control 
+                type = "number" 
+                value = { hourCount }
+                name = "hourCount"
+                onChange = { onChange }
+            />
+        </Form.Group>
         <Button onClick = { onSubmit }>Next</Button>
     </div>
 )
--- src/containers/App/InputForm/index.js
+++ src/containers/App/InputForm/index.js
@@ -12,7 +12,9 @@ class InputForm extends Component {
         displayBingePeriodForm: true,
         displayPlatformForm: false,
         displayBingeThemeForm: false,
-        progress: 1/3
+        progress: 1/3,
+        dayCount: 0,
+        hourCount: 0
     }
 
     onSubmitBingePeriodForm = () => {
@@ -61,14 +63,24 @@ class InputForm extends Component {
         })
     }
 
+    onInputFieldChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
     render(){
         const { displayBingePeriodForm, displayPlatformForm, displayBingeThemeForm, progress } = this.state;
+        const { dayCount, hourCount } = this.state;
         return (
             <div className = "InputForm">
                 <ProgressBar now = { progress } max = { 1 } />
                 <Form onSubmit = { this.onSubmitForm.bind(this) }>
                     { displayBingePeriodForm ? (
                         <BingePeriodForm 
+                            dayCount = { dayCount }
+                            hourCount = { hourCount }
+                            onChange = { this.onInputFieldChange.bind(this) }
                             onSubmit = { this.onSubmitBingePeriodForm.bind(this) }
                         />
                     ) : null }
